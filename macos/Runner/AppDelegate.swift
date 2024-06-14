1ac8942757011a7dc8c006af33fbf42b927638b6
Kim Chi <45139505+dnkchi2809@users.noreply.github.com>
2024-06-14 14:45:01 +0700

    feat: set minimum window size

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -6,4 +6,13 @@ class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return true
   }
+
+  override func applicationDidFinishLaunching(_ aNotification: Notification) {
+    if let window = NSApplication.shared.windows.first {
+      window.setContentSize(NSSize(width: 1200, height: 800)) // Set your initial window size here
+      window.minSize = NSSize(width: 1200, height: 800) // Set your minimum window size here
+//      window.maxSize = NSSize(width: 1920, height: 1080) // Set your maximum window size here
+    }
+    super.applicationDidFinishLaunching(aNotification)
+  }
 }
